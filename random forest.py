853c384f5417ac3d95c8dd9207cd18b9dc7458e1
KIRANKUMARS3201 <127059808+KIRANKUMARS3201@users.noreply.github.com>
2023-04-24 16:53:41 +0530

    Add files via upload

---
+++ random forest.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+
+# Load the EMG data set into a pandas dataframe
+emg_data = pd.read_csv('/content/ML_Pattern_Recognition_Data V2.csv')
+
+# Split the data set into features (X) and labels (y)
+X = emg_data.iloc[:, 1:]
+y = emg_data.iloc[:, 0]
+
+# Create the Random Forest model
+clf = RandomForestClassifier()
+
+# Train the model on the EMG data
+clf.fit(X, y)
+
+# Predict labels for the training set
+y_pred = clf.predict(X)
+
+# Calculate accuracy of the model on the training set
+accuracy = accuracy_score(y, y_pred)
+print("Accuracy on training set: {:.2f}%".format(accuracy * 100))
+
+# Save the model as a pickle file
+filename = 'emg_model.pkl'
+pickle.dump(clf, open(filename, 'wb'))
